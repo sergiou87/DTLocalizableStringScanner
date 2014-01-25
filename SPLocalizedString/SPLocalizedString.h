bf0deff7511ed9357dbf50d73e63176aedaa0fa4
Sergio Padrino <sergio.padrino@gmail.com>
2014-01-25 14:13:19 +0100

    New dir structure

---
+++ SPLocalizedString/SPLocalizedString.h
@@ -0,0 +1,13 @@
+//
+//  SPLocalizedString.h
+//  genstrings2
+//
+//  Created by Sergio Padrino on 25/01/14.
+//  Copyright (c) 2014 Drobnik KG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SPLocalizedString : NSObject
+
+@end
---
+++ SPLocalizedString/SPLocalizedString.m
@@ -0,0 +1,13 @@
+//
+//  SPLocalizedString.m
+//  genstrings2
+//
+//  Created by Sergio Padrino on 25/01/14.
+//  Copyright (c) 2014 Drobnik KG. All rights reserved.
+//
+
+#import "SPLocalizedString.h"
+
+@implementation SPLocalizedString
+
+@end
